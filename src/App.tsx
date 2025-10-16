e5299385401169fbfa4b4413a554731d5b145804
Jeff-003 <1527704074@qq.com>
2025-10-16 09:54:42 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,6 @@ function App() {
             </div>
           </div>
           <div className="flex items-center space-x-4">
-            <button className="text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-colors">
-              <Bell className="w-5 h-5" />
-            </button>
             <button className="relative text-gray-500 hover:text-gray-700">
               <Bell className="w-5 h-5" />
               <span className="absolute -top-1 -right-1 w-4 h-4 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">1</span>
